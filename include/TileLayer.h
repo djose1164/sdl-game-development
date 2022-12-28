e90723ef64739a0aa46dd97744c5bd2f456bc417
Daniel Victoriano <djose1164@gmail.com>
2022-12-28 17:34:44 -0400

    Add TileLayer header.

---
+++ include/TileLayer.h
@@ -0,0 +1,34 @@
+#ifndef TILE_LAYER_H
+#define TILE_LAYER_H
+
+#include "Level.h"
+#include "Vector2D.h"
+
+class TileLayer : public Layer
+{
+public:
+    TileLayer(int tileSize, std::vector<Tileset> &tilesets);
+
+    void update() override;
+    void render() override;
+
+    void setTileIds(const std::vector<std::vector<int>> &data);
+
+    void tileSize(int tileSize);
+
+    auto TilesetById(int tileId);
+
+private:
+    int numColumns_;
+    int numRows_;
+    int tileSize_;
+
+    Vector2D position_;
+    Vector2D velocity_;
+
+    const std::vector<Tileset> &tilesets_;
+
+    std::vector<std::vector<int>> tileIds_;
+};
+
+#endif //TILE_LAYER_H
